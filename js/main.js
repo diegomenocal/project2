1b63f0225cc12f6e15e1f05a2d836a2667a957b5
diegomenocal <menocaldiego@gmail.com>
2016-12-06 22:57:42 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,34 +1,46 @@
 $(window).scroll(function(){
 
+  var topNumber = $(window).scrollTop();
 
-  //var topNumber = $(window).scrollTop();
+  var topNumber = Math.floor( $(window).scrollTop() );
 
-  var topNumber = Math.floor( $(window).scrollTop()
+  $("#counter").text(topNumber);
 
-  );
+  $("#counter").text(topNumber);
 
-  //$("#counter3").text(topNumber);
+ $("#counter").css({
 
-  $("#mountain1").css({
-    "top":topNumber * -1.0 +"px"
+   "color":"white",
+   "background-color":"blue",
+   "top":30 + topNumber * 0 +"px"
 
-  });
+ });
 
+  $("#mountain1").css({"top":topNumber * -1.0 +"px"});
 
-  $("#mountain2").css({
-    "top":topNumber * -0.5 +"px"
+  $("#mountain2").css({"top":topNumber * -0.5 +"px"});
 
-  });
-
-  $("#mountain3").css({
-
-    "top":topNumber * 0 +"px"
-
-  });
+  $("#mountain3").css({"top":topNumber * 0 +"px"});
 
 
-  //console.log(topNumber);
+  if ($(this).scrollTop() > 1000) {
+          $(".event").css({"opacity": "1"});
+      } else {
+          $(".event").css({"opacity": "0"});
+      }
 
+  if ($(this).scrollTop() < 10000) {
+          $(".event").css({"opacity": "1"});
+      } else {
+          $(".event").css({"opacity": "0"});
+      }
 
+  if ($(this).scrollTop() > 10000) {
+          $(".event2").css({"opacity": "1"});
+      } else {
+          $(".event2").css({"opacity": "0"});
+      }
+
+  console.log(topNumber);
 
 });
